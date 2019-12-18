9ec24e98277d8ccdd83627367ebfa19e5b3e82f8
Steven "Grant" Carey <careyst1@msu.edu>
2019-12-18 10:13:41 -0500

    Initial Commit

---
+++ Tippy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tippy
+//
+//  Created by Steven Carey on 12/18/19.
+//  Copyright © 2019 Steven Carey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
